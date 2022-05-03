0e996e97d924517c8ef2ddd792c7285920229fa7
Steven Love <lovestevend@gmail.com>
2022-05-03 14:40:14 -0400

    wordle line2

--- wordle/app/pages/home/home.svelte
+++ wordle/app/pages/home/home.svelte
@@ -11,17 +11,17 @@
 	import Menu from "@components/common/menu/menu.svelte";
 	import Footer from "@components/common/footer/footer.svelte";
 	import { translate } from "@app/translations/translate";
-	import {wordList} from "./home";
+	import {wordList,dictionary} from "./home";
 	
 	let wordToTest = "hi"
 
-	function isInWordList(word:String):Boolean{
+	function isInWordList(word:String,listToSearch:string):Boolean{
 		word = word.toLowerCase();
 		const regex = RegExp("^"+word+"$","gm")
 		let m;
 		let found = false;
 		if(word.length<5)return false;
-		while ((m = regex.exec(wordList)) !== null) {
+		while ((m = regex.exec(listToSearch)) !== null) {
 			// This is necessary to avoid infinite loops with zero-width matches
 			if (m.index === regex.lastIndex) {
 				regex.lastIndex++;
@@ -36,11 +36,19 @@
 		return found;
 	}
 	function getDescription(word:String):String{
-		let bool = isInWordList(word)
+		let bool = isInWordList(word,wordList)
 		if(bool){
-			return "✅ " + word + " is a wordle word"
+			return "✅ " + word + " can be an answer in wordle"
 		}else{
-			return "❌ "+word + " is not a wordle word"
+			return "❌ "+word + " can't be an answer in wordle"
+		}
+	}
+	function getDescription2(word:String):String{
+		let bool = isInWordList(word,dictionary);
+		if(bool){
+			return "✅ " + word + " is in the wordle dictionary" 
+		}else{
+			return "❌ "+word + " is not in the wordle dictionary"
 		}
 	}
 </script>
@@ -49,6 +57,7 @@
 
 <input bind:value={wordToTest} maxlength="5" autocapitalize="false"/>
 <h3>{getDescription(wordToTest)}</h3>
+<h3>{getDescription2(wordToTest)}</h3>
 
 <!-- <div id="container">
 	<section class="hero is-medium is-primary is-bold">
